1941f9b84fd33c6735de856ab0f304faff57f77f
parseltongist <>
2022-01-05 11:54:00 +0100

    Stage 3/5

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -7,13 +7,28 @@ from random import choice
 
 class TicTacToe:
 
-    def __init__(self):
+    def __init__(self, player1, player2):
         self.curr_player_sign = "X"  # default value as "X" makes the 1st move
         self.grid = [[' ', ' ', ' '] for _ in range(3)]
         self.top, self.mid, self.bot = \
             self.grid[0], self.grid[1], self.grid[2]
         self.mode = "easy"
         self.state = "in process"
+        self.player1 = player1
+        self.player2 = player2
+
+    @classmethod
+    def from_string(cls):
+        game_parameters = input("Input command: ")
+        if game_parameters not in ("exit", "start easy user", "start user easy", "start user user", "start easy easy"):
+            print("Bad parameters!")
+            return TicTacToe.from_string()
+        elif game_parameters == "exit":
+            exit()
+        else:
+            player_1, player_2 = \
+                game_parameters.split()[1], game_parameters.split()[2]
+            return cls(player_1, player_2)
 
     def current_state_analyzer(self):
         x_counter = 0
@@ -88,33 +103,23 @@ class TicTacToe:
             self.state = "Finished"
             return main()
 
-    def gameplay(self, _player1, _player2):
-        self.user_move() if _player1 == "user" else self.computer_move()
+    def gameplay(self):
+        self.user_move() if self.player1 == "user" else self.computer_move()
         self.game_rules_validation()
         self.switch_player()
-        self.user_move() if _player2 == "user" else self.computer_move()
+        self.user_move() if self.player2 == "user" else self.computer_move()
         self.game_rules_validation()
         self.switch_player()
-
         # play with "current player to save 2 lines"?
 
 
 # make it @classmethod ?
 def main():
-    game_parameters = input("Input command: ")
-    if game_parameters not in ("exit", "start easy user", "start user easy", "start user user", "start easy easy"):
-        print("Bad parameters!")
-        return main()
-    elif game_parameters == "exit":
-        exit()
-    else:
-        game = TicTacToe()
+    while True:
+        game = TicTacToe.from_string()
         game.print_grid()  # print 1st clear grid/field
         while game.state != "Finished":
-            player1, player2 =\
-                game_parameters.split()[1], game_parameters.split()[2]
-            game.gameplay(player1, player2)
-        return main()
+            game.gameplay()
 
 
 if __name__ == '__main__':
